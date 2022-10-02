1b96853478db22f3be805868ebc6a66d6bea4301
Meorwik <ilapolienko77@gmail.com>
2022-10-02 18:04:27 +0600

    some small changes

--- handlers/users/category.py
+++ handlers/users/category.py
@@ -4,13 +4,17 @@ from states.states import ChooseCategory
 from loader import dp
 
 
-@dp.message_handler(commands=['category'])
-async def show_available_categories(message: types.Message):
+async def something(message):
     text = ''
     num = 0
-    await message.answer("Этот бот имеет в себе очень много разных категорий, вы можете выбрать любую из ниже перечисленных:")
     for category in CategoryList:
-        num +=1
+        num += 1
         text += f"{num}: {category}\n"
     await message.answer(text)
+
+
+@dp.message_handler(commands=['category'])
+async def show_available_categories(message: types.Message):
+    await message.answer("Этот бот имеет в себе очень много разных категорий, вы можете выбрать любую из ниже перечисленных:")
+    await something(message)
     await ChooseCategory.stateChoosingCat.set()
--- handlers/users/send_pictures.py
+++ handlers/users/send_pictures.py
@@ -1,8 +1,14 @@
 from aiogram import types
+
+import states.states
 from .web_requests import PictureGeter, CategoryList
 from loader import dp, bot
 from states.states import ChooseCategory
 from aiogram.dispatcher import FSMContext
+from aiogram.utils.exceptions import InvalidHTTPUrlContent, RetryAfter
+from keyboards.default.new_keyboard import keyboard
+from aiogram.dispatcher.filters import Text, Command
+from .category import something
 
 temp: str = ""
 
@@ -13,7 +19,7 @@ async def set_category(message: types.Message):
     for_check = message.text.capitalize()
     if for_check in CategoryList:
         await message.answer(
-            f"Вы выбрали: {message.text}\nТеперь укажите количество картинок которое хотите получить (не больше 1000)")
+            f"Вы выбрали: {message.text}\nТеперь укажите количество картинок которое хотите получить (не больше 100)")
         temp = for_check
         await ChooseCategory.stateChoosingCount.set()
     else:
@@ -24,11 +30,24 @@ async def set_category(message: types.Message):
 async def set_count(message: types.Message, state: FSMContext):
     global temp
     parsed_pictures = PictureGeter.get_list_with_pictures(int(message.text), temp)
-
+    print(parsed_pictures)
     for url_link in parsed_pictures:
-        await bot.send_photo(chat_id=message.chat.id, photo=url_link)
-
+        try:
+            await bot.send_photo(chat_id=message.chat.id, photo=url_link)
+        except InvalidHTTPUrlContent:
+            pass
     await state.finish()
+    await message.answer("Хотите выбрать еще одну категорию ?", reply_markup=keyboard)
 
 
+@dp.message_handler(Text(equals=["Да"]))
+async def reply_on(message: types.Message):
+    await message.answer("Как пожелаете =)", reply_markup=types.ReplyKeyboardRemove())
+    await something(message)
+    await ChooseCategory.stateChoosingCat.set()
+
+
+@dp.message_handler(Text(equals=["Нет"]))
+async def reply_on(message: types.Message):
+    await message.answer("Мы благодарны за то что вы используете нашего бота ;)", reply_markup=types.ReplyKeyboardRemove())
 
