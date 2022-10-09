70918b67e6bf9736388207f8e2555402125e6b0d
Meorwik <ilapolienko77@gmail.com>
2022-10-09 19:28:25 +0600

    даже не знаю

--- utils/db_api/db_api.py
+++ utils/db_api/db_api.py
@@ -1,10 +1,23 @@
 import sqlite3
 
 
-def connect():
-    global cur, con
-    con = sqlite3.connect("../../data/DataBase/quiz.db")
-    cur = sqlite3.Cursor()
+def connect(db_name):
+    con = sqlite3.connect(f"../../data/DataBase/{db_name}.db")
+    cur = sqlite3.Cursor(con)
+    return con, cur
 
-def add_some_info():
-    pass
+
+def get_some_info(db_name, get_what, get_from):
+    connection, cursor = connect(db_name)
+    connection.execute(f"SELECT {get_what} FROM {get_from}")
+    print(cursor.fetchall())
+
+
+def add_some_info(db_name, add_what, add_where):
+    connection, cursor = connect(db_name)
+    connection.execute(f"INSERT INTO {add_where} VALUES({add_what})")
+
+
+add_some_info("users_logs", "UserName", "users")
+
+get_some_info("users_logs", "*", "users")
